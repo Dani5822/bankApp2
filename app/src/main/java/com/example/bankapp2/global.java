d68a6ca2b40806c111bca01ff9fa4e4482fb6c6d
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2025-04-14 14:28:19 +0200

    javadoc

--- app/src/main/java/com/example/bankapp2/global.java
+++ app/src/main/java/com/example/bankapp2/global.java
@@ -2,22 +2,45 @@ package com.example.bankapp2;
 
 import android.app.Application;
 
+/**
+ * The type Global.
+ */
 public class global extends Application {
     private String id;
     private String access_token;
 
+    /**
+     * Gets access token.
+     *
+     * @return the access token
+     */
     public String getAccess_token() {
         return access_token;
     }
 
+    /**
+     * Sets access token.
+     *
+     * @param access_token the access token
+     */
     public void setAccess_token(String access_token) {
         this.access_token = access_token;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(String id) {
         this.id = id;
     }
